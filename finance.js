a270ccb8111ae627b158c47abb253c046931403a
house-of-muta <murata.murata.muta@gmail.com>
2026-03-13 10:28:48 +0900

    finance.js

--- finance.js
+++ finance.js
@@ -1,14 +1,15 @@
 const axios=require("axios")
 
-async function stock(event){
+async function stock(client,event){
 
-const res=await axios.get(
-"https://query1.finance.yahoo.com/v8/finance/chart/^N225"
-)
+const res=await axios.get("https://query1.finance.yahoo.com/v8/finance/chart/^N225")
 
 const price=res.data.chart.result[0].meta.regularMarketPrice
 
-return price
+return client.replyMessage(event.replyToken,{
+type:"text",
+text:`日経平均\n${price}円`
+})
 
 }
 
