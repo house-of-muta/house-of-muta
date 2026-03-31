f8dcdd60abdcea4e2816e24631e1138231283a07
house-of-muta <murata.murata.muta@gmail.com>
2026-04-01 07:40:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express()
 
 const config = {
   channelAccessToken: process.env.LINE_ACCESS_TOKEN,
-  channelSecret: process.env.LINE_SECRET
-}
+  channelSecret: process.env.LINE_CHANNEL_SECRET
+};
 
 const client = new line.Client(config)
 
